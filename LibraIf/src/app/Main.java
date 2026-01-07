6485a9d51e2cab8f1163f746ddf074e28751cb77
Breno Israel <brenoisrael931@gmail.com>
2026-01-07 15:10:41 -0300

    Criando as classes inicias, sendo elas: Main, Usuario, Biblioteca e Livro

---
+++ LibraIf/src/app/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Esta classe é responsável por iniciar a aplicação da biblioteca libraIf.
+ * Aqui serão realizados testes e chamadas aos métodos do sistema.
+*/
+
+package app;
+
+public class Main {
+    
+}
