1bce507754585a016856c15d8dc9b6e6ffe6e4b6
Breno Israel <brenoisrael931@gmail.com>
2026-01-07 15:25:49 -0300

    Definindo os Getters e os Setters

--- LibraIf/src/model/Livro.java
+++ LibraIf/src/model/Livro.java
@@ -6,4 +6,38 @@ public class Livro {
     private String autor;
     private boolean disponivel;
 
+    /* Definindo os Getter */
+    public String getAutor() {
+        return autor;
+    }
+    
+    public String getISBN() {
+        return ISBN;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    
+    /*Definindo os Setters */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    public void setISBN(String iSBN) {
+        ISBN = iSBN;
+    }
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+    
 }
--- LibraIf/src/model/Usuario.java
+++ LibraIf/src/model/Usuario.java
@@ -3,4 +3,23 @@ package model;
 public class Usuario {
     private String nome;
     private int matricula;
+
+    /* Definindo os Getters */
+    public String getNome() {
+        return nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    /* Definindo os Setters */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    
 }
