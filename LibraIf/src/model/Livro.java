8c46113e0bb2a0511cf3da3958464d3b1d60f4d4
Breno Israel <brenoisrael931@gmail.com>
2026-01-08 15:12:13 -0300

    reformulando a classe livro

--- LibraIf/src/model/Livro.java
+++ LibraIf/src/model/Livro.java
@@ -6,42 +6,48 @@ package model;
 
 public class Livro {
     private String titulo;
-    private String ISBN;
     private String autor;
+    private String isbn;
+    private int anoPublicacao;
     private boolean disponivel;
 
-    /* Definindo os Getter */
-    public String getAutor() {
-        return autor;
-    }
-    
-    public String getISBN() {
-        return ISBN;
-    }
-    
     public String getTitulo() {
         return titulo;
     }
-    
-    public boolean isDisponivel() {
-        return disponivel;
-    }
-    
-    /*Definindo os Setters */
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-    
-    public void setISBN(String iSBN) {
-        ISBN = iSBN;
-    }
-    
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
     public void setDisponivel(boolean disponivel) {
         this.disponivel = disponivel;
     }
-    
 }
